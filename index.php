76d1ec748a05a729d374f2cce6f9a97102b66941
Merouze Aurelien <aurelienmerouze@gmail.com>
2023-11-07 14:05:45 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">    
+    <link rel="stylesheet" href="style.css">
+    <title>My to-do</title>
+</head>
+<body>
+    <div>
+    <h1>My To-Do</h1>
+    </div>
+
+    <form action="" method="get">
+        <input type="text" name="name_product" id="name_field" placeholder="Add a task" required>
+        <input type="submit" value="+">
+    </form>
+    
+    
+</body>
+</html>
