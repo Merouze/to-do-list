0e5bc0e7cd737efab0856da0db71636baa19b609
Merouze Aurelien <aurelienmerouze@gmail.com>
2023-11-07 15:18:54 +0100

    displai task from dtb

--- index.php
+++ index.php
@@ -1,39 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=
-    , initial-scale=1.0">    
+    , initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>My to-do</title>
 </head>
-    <?php
-    try {
-        $dbMtdl = new PDO (
-            'mysql:host=localhost;dbname=todo_list;charset=utf8',
-            'root'
-        );
-    $dbMtdl->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,
-    PDO::FETCH_ASSOC);
-    }
-    catch (Exception $e) {
+<?php
+try {
+    $dbMtdl = new PDO(
+        'mysql:host=localhost;dbname=todo_list;charset=utf8',
+        'root'
+    );
+    $dbMtdl->setAttribute(
+        PDO::ATTR_DEFAULT_FETCH_MODE,
+        PDO::FETCH_ASSOC
+    );
+} catch (Exception $e) {
     die('Unable to connect to the database.
-    '.$e->getMessage());
-    }    
-    ?>
+    ' . $e->getMessage());
+}
+?>
+
 <body>
-    
-    <?php
-    ?>
     <div>
         <h1>My To-Do</h1>
     </div>
-    
+
     <form action="" method="get">
         <input type="text" name="name_product" id="name_field" placeholder="Add a task" required>
         <input type="submit" value="+">
     </form>
-    
-</body>
-</html>
+    <ul>
+    <?php
+    $query = $dbMtdl->prepare("SELECT task FROM task;");
+    $query->execute();
+    $result = $query->fetchAll();
+    foreach ($result as $task) {
+        echo '<li>' . $task['task'] . '</li>';
+    }
+    ?>
+    </ul>
 
+    <?php
+
+
+    ?>
+
+</body>
+
+</html>
