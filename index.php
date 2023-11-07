6eeb02646556307505d3e5a86244296be7b365fe
Merouze Aurelien <aurelienmerouze@gmail.com>
2023-11-07 14:59:05 +0100

    link with dtb

--- index.php
+++ index.php
@@ -7,16 +7,33 @@
     <link rel="stylesheet" href="style.css">
     <title>My to-do</title>
 </head>
+    <?php
+    try {
+        $dbMtdl = new PDO (
+            'mysql:host=localhost;dbname=todo_list;charset=utf8',
+            'root'
+        );
+    $dbMtdl->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,
+    PDO::FETCH_ASSOC);
+    }
+    catch (Exception $e) {
+    die('Unable to connect to the database.
+    '.$e->getMessage());
+    }    
+    ?>
 <body>
+    
+    <?php
+    ?>
     <div>
-    <h1>My To-Do</h1>
+        <h1>My To-Do</h1>
     </div>
-
+    
     <form action="" method="get">
         <input type="text" name="name_product" id="name_field" placeholder="Add a task" required>
         <input type="submit" value="+">
     </form>
     
-    
 </body>
 </html>
+
