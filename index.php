7ad7f186426ac160aad2aed4eba71274299dabfb
Merouze Aurelien <aurelienmerouze@gmail.com>
2023-11-08 12:09:35 +0100

    add task in database

--- index.php
+++ index.php
@@ -14,40 +14,40 @@ try {
     die('Unable to connect to the database.
     ' . $e->getMessage());
 }
-// ***************** display li ********************************
-$query = $dbMtdl->prepare("SELECT task FROM task ORDER BY date_create ASC;");
-$query->execute();
-$result = $query->fetchAll();
 // ******************* add with input ***********************
 if(isset($_POST['task'])) {
-$task = (strip_tags($_POST['task']));
-$addList = $dbMtdl->prepare("INSERT INTO `task` (`task`) VALUES (:task)");
-$addList->execute([
-    'task' => $task
-]);
-if ($addList->rowCount()) {
-    $msg[] = 'tâche ajoutée';
-}};
-?>
+    $task = (strip_tags($_POST['task']));
+    $addList = $dbMtdl->prepare("INSERT INTO `task` (`task`) VALUES (:task)");
+    $addList->execute([
+        'task' => $task
+    ]);
+    if ($addList->rowCount()) {
+        $msg[] = 'tâche ajoutée';
+    }};
+    ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=
-    , initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>My to-do</title>
-</head>
-<body>
-    <div>
-        <h1>My To-Do</h1>
-    </div>
-    <form action="" method="POST">
-        <input type="text" name="task" id="name_field" placeholder="Add a task" required>
-        <input type="submit" value="+">
-    </form>
-    <ul>
-        <?php
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=
+        , initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>My to-do</title>
+    </head>
+    <body>
+        <div>
+            <h1>My To-Do</h1>
+        </div>
+        <form action="" method="POST">
+            <input type="text" name="task" id="name_field" placeholder="Add a task" required value="">
+            <input type="submit" value="+">
+        </form>
+        <ul>
+            <?php
+            // ***************** display li ********************************
+            $query = $dbMtdl->prepare("SELECT task FROM task ORDER BY date_create DESC;");
+            $query->execute();
+            $result = $query->fetchAll();
         foreach ($result as $task) {
             echo '<div><li><input type="checkbox" name="" id="">' . $task['task'] . '</li></div>';
         }
@@ -55,6 +55,5 @@ if ($addList->rowCount()) {
     </ul>
     <?php
     ?>
-    <div><button>supprimer</button></div>
 </body>
 </html>
